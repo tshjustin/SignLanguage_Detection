2254ce9f57f14ba3e4f666f2a84440c6e0417665
Justin <j.tann2305@gmail.com>
2024-03-18 17:28:31 +0800

    added data collection method

--- action_collection.py
+++ action_collection.py
@@ -25,7 +25,7 @@ def create_folder():
             except:
                 pass
 
-def train(actions, sequence_length, no_sequences):
+def collection(actions, sequence_length, no_sequences):
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic: # set mediapipe model  - set initial detection conf then subsequent tracking conf 
         for action in actions:
         # Loop through videos
@@ -49,7 +49,7 @@ def train(actions, sequence_length, no_sequences):
                         cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
                                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
                         cv2.imshow('OpenCV Feed', image)
-                        cv2.waitKey(2000) # break 
+                        cv2.waitKey(500) # break 
                         
                     else: 
                         cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15,12), 
@@ -62,9 +62,11 @@ def train(actions, sequence_length, no_sequences):
                     npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
                     np.save(npy_path, keypoints)
 
-                    # Break gracefully
+                    # Break with 'q'
                     if cv2.waitKey(10) & 0xFF == ord('q'):
                         break
-
+    cap.release()
+    cv2.destroyAllWindows()
+                        
 if __name__ == '__main__':
-    create_folder()
+    collection(actions, sequence_length, no_sequences)
